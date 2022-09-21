936107eed31dd1ac4bceb8373927666843d7e62e
Mimi-Afsana <afsanamimi61220@gmail.com>
2022-09-21 18:49:46 +0800

    added Router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 import Login from "./components/share/login/Login";
 import Register from "./components/share/register/Register";
@@ -6,9 +7,12 @@ import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
-    <>
-      <Login></Login>
-    </>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/register" element={<Register></Register>}></Route>
+      <Route path="/profile/:username" element={<Profile></Profile>}></Route>
+    </Routes>
   );
 }
 
